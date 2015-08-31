9dc3837630c8fb3e6a3cfabdc766765f8f7cf898
williampsena <williampsena@gmail.com>
2015-08-31 00:31:46 +0000

    Hello World Yeoman

---
+++ NodeJs/Hello-World-Yeoman/client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('helloWorldYeomanApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
